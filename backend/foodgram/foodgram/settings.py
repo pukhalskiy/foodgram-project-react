657823f12c6f6cc0c48e1ba0ed0db3464e81b472
unknown <ego3200@gmail.com>
2023-12-21 21:50:01 +0300

    nginx and compose settings

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -13,12 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('HOSTS', '').split(',')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*', 'backend']
+ALLOWED_HOSTS = os.getenv('HOSTS', '').split(',')
 
 AUTH_USER_MODEL = 'users.User'
 
