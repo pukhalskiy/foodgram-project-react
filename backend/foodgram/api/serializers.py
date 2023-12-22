553cc886406e6c477e7f01856c2cc04351c525fc
unknown <ego3200@gmail.com>
2023-12-23 00:31:06 +0300

    flake8 2

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -169,8 +169,8 @@ class RecipeListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipes
         fields = ('id', 'tags', 'author', 'ingredients',
-                  'is_favorited',  'is_in_shopping_cart',
-                  'name', 'image', 'text', 'cooking_time')
+                  'is_favorited',  'is_in_shopping_cart', 'name', 'image',
+                  'text', 'cooking_time')
 
     def get_is_favorited(self, obj):
         user = self.context.get('request').user
